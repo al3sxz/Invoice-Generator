cbacdf841ecf8562547ef2ff1a1decd0686fe5e3
al3sxz <4lezsierra@gmail.com>
2026-04-01 13:49:51 -0500

    router.php

--- router.php
+++ router.php
@@ -1,5 +1,31 @@
 <?php
+
 $uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+
+
+if (strpos($uri, '/storage/') === 0) {
+    $filePath = __DIR__ . '/storage/app/public' . substr($uri, 8);
+    if (file_exists($filePath) && is_file($filePath)) {
+        $ext = pathinfo($filePath, PATHINFO_EXTENSION);
+        $types = [
+            'jpg' => 'image/jpeg',
+            'jpeg' => 'image/jpeg',
+            'png' => 'image/png',
+            'gif' => 'image/gif',
+            'svg' => 'image/svg+xml',
+            'pdf' => 'application/pdf',
+            'woff' => 'font/woff',
+            'woff2' => 'font/woff2',
+        ];
+        if (isset($types[$ext])) {
+            header('Content-Type: ' . $types[$ext]);
+        }
+        header('Cache-Control: public, max-age=31536000');
+        readfile($filePath);
+        exit;
+    }
+}
+
 
 if ($uri !== '/' && file_exists(__DIR__ . '/public' . $uri)) {
     $ext = pathinfo($uri, PATHINFO_EXTENSION);
